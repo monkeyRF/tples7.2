b79a5e83ebd26d723fa0958df4e3b5e556eb36ab
Philipp <trushin.philipp@gmail.com>
2022-05-08 22:57:00 +0300

    Плавное изменение цвета и размера прямоугольников

--- app/src/main/java/com/example/tples72/FragmentOne.kt
+++ app/src/main/java/com/example/tples72/FragmentOne.kt
@@ -1,7 +1,10 @@
 package com.example.tples72
 
+import android.animation.AnimatorSet
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.Color
 import android.os.Bundle
-import android.transition.*
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,21 +13,54 @@ import android.widget.Button
 
 class FragmentOne : Fragment() {
 
+    private lateinit var rectangleOne: View
+    private lateinit var rectangleTwo: View
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_one, container, false).also {
-        it.findViewById<Button>(R.id.click_me_button).setOnClickListener {
-            expand()
+    ): View? = inflater.inflate(R.layout.fragment_one, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rectangleOne = view.findViewById(R.id.rect_1)
+        rectangleTwo = view.findViewById(R.id.rect_2)
+
+        view.findViewById<Button>(R.id.click_me_button).setOnClickListener {
+            animation()
         }
     }
 
-    private fun expand() {
-        var scene = Scene.getSceneForLayout(requireView() as ViewGroup, R.layout.scene_2,requireContext())
-        TransitionManager.go(scene, TransitionSet().apply {
-            duration = 400L
-            addTransition(ChangeBounds())
-        })
+    private fun animation() {
+
+        val colorAnimator = ValueAnimator
+            .ofInt(Color.parseColor("#FF6200EE"), Color.parseColor("#FFFF2903"))
+            .setDuration(400)
+        colorAnimator.addUpdateListener { valueAnimator ->
+            rectangleOne.setBackgroundColor((valueAnimator.animatedValue as Int))
+        }
+        colorAnimator.setEvaluator(ArgbEvaluator())
+
+        val rectOneSizeAnimator = ValueAnimator.ofInt(rectangleOne.height, 60).setDuration(400)
+        rectOneSizeAnimator.addUpdateListener { valueAnimator ->
+            val size = valueAnimator.animatedValue as Int
+            rectangleOne.layoutParams.width = size
+            rectangleOne.layoutParams.height = size
+            rectangleOne.requestLayout()
+        }
+
+        val rectTwoSizeAnimator = ValueAnimator.ofInt(rectangleOne.height, 120).setDuration(400)
+        rectTwoSizeAnimator.addUpdateListener { valueAnimator ->
+            val size = valueAnimator.animatedValue as Int
+            rectangleTwo.layoutParams.height = size
+            rectangleTwo.requestLayout()
+        }
+
+        AnimatorSet().apply{
+            play(colorAnimator).with(rectOneSizeAnimator).with(rectTwoSizeAnimator)
+            start()
+        }
     }
 
     companion object {
